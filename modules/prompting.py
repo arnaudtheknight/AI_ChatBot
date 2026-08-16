ab76e6abc8fb76d6ec56048eb157a3b16ea5f3f6
arnaudtheknight <arnaudtheknight@gmail.com>
2026-08-16 03:14:10 +0200

    adding function to better use previous history for current prompts

---
+++ modules/prompting.py
@@ -0,0 +1,31 @@
+from classes import log2
+
+def prompting2(previous=""):
+    if previous:
+        return previous
+    contents = []
+    while True:
+        try:
+            prompt = str(input("[Press Ctrl-D to end] Prompt here: "))
+            contents.append(prompt)
+        except EOFError:
+            break
+    text = '\n'.join(contents)
+    return text
+
+def prompting(previous=""):
+    if previous:
+        return previous
+    inputs = []
+    print(f"Enter your prompt here, or press Ctrl-D to finish: \n{log2.NEG_GREY}")
+    while True:
+        try:
+            line = str(input())
+        except EOFError:
+            print(f"END OF LINE.{log2.RESET}")
+            break
+    prompt = '\n'.join(inputs)
+    return prompt
+
+if __name__ == "__main__":
+    print(prompting())
