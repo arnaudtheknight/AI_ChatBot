6d350eba6e5183f011d58230ca1f113c10ace2dc
arnaudtheknight <arnaudtheknight@gmail.com>
2026-08-29 17:11:25 +0200

    changed filenames around

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,7 +1,7 @@
 from .chat_processes import interact, interact_stream, generate, generate_stream
 from .classes import log, log2, slash
 from .config import configuration
-from .history2 import history_export, history_import
+from .history import history_export, history_import
 from .model_list import model_lister
 from .prompts import prompting
 
--- modules/history.py
+++ modules/history.py
@@ -1,60 +1,88 @@
-# Importing modules:
-from sys import exit
-from os import getcwd as get_path
-from json import load, dump
+## Importing modules:
+# Quit time:
 from datetime import datetime
-## Self-written:
-from modules.classes import log, log2
+# Folder gen:
+from os.path import isdir
+from os import makedirs
+# Self-written modules:
+from modules.classes import log2, slash
+# Global:
+from os import getcwd as get_path
+from json import JSONDecodeError, load, dump
+from sys import exit
 
-# Import previous chat history from JSON file:
-def history_import():
-    root = get_path()
+# Obtain file contents:
+def get_contents(path):
+    with open(path, 'r') as file:
+        content = load(file)
+        print(log2.Importing.Import_Success)
+        return content
+
+# History import logic:
+def history_import(root=get_path()):
     while True:
-        print(log2.Import_Prompt)
-        path = input(f"{log2.UNDER}{root}/{log2.RESET}") # [./]history/blah.json
-        if path.lower() in {'', 'n'}:
-            print(log2.Import_Skipped)
+        print(log2.Importing.Import_Prompt)
+        path = input(f"{log2.UNDER}{root}/{log2.RESET}") # [/.../]history/blah.json
+
+        # Skip import:
+        if path.lower() in slash.History_Skip:
+            print(log2.Importing.Import_Skipped)
             return []
-        else:
-            path = f"/{path}"
+
         candidates = [
-            f"{root}{path}", # [root/]history/blah.json
-            f"{root}/history{path}", # [root/history/]blah.json
-            path # ~/.../history/blah.json
+            f"{root}/{path}", # input: history/blah.json
+            f"{root}/history/{path}", # input: blah.json
+            f"/{path}" # input: /.../history/blah.json
         ]
+
         for spot in candidates:
             try:
-                with open(spot, 'r') as file:
-                    content = load(file)
-                print(log.Hist_Import_Success)
-                return content
-            except FileNotFoundError:
-                print(f"Tried opening '{log2.UNDER}{spot}{log2.RESET}' and failed.")
-        print(log2.Import_Failed)
+                return get_contents(spot)
+            except FileNotFoundError as err:
+                print(f"{log2.Importing.Import_Error(spot)} {err.strerror}. ")
+            except JSONDecodeError as err:
+                print(log2.Importing.Import_Error(spot), log2.Importing.Import_Error_JSON)
+        print(log2.Importing.Import_Failure)
 
-# Export this chat as JSON file:
-def history_export(history):
-    root = get_path()
+# Get exit time:
+def quit_handling():
     now = datetime.now()
     stop_d, stop_h, stop_m, stop_s = now.date(), f"{now.hour:02d}", f"{now.minute:02d}", f"{now.second:02d}"
-    stop = f"{stop_d}-{stop_h}{stop_m}{stop_s}"
-    name1, name2, name_true = f"{root}/history/hist-{stop}.json", f"{root}/hist-{stop}.json", None
-    for name in (name1, name2):
-        try:
-            with open(name, 'a') as file:
-                dump(history, file, ensure_ascii=False, indent=2)
-            name_true = name
-            break
-        except Exception:
-            print(log2.Export_Failure)
-            print(log2.Export_Alternative)
-    print(f"\n{log2.INFO} Exited on {stop_d} at {stop_h}:{stop_m}:{stop_s}.{log2.RESET} ")
-    exit(f"{log2.INFO} Exported to {log2.UNDER}{name_true}.{log2.RESET} ")
+    name = f"{stop_d}-{stop_h}{stop_m}{stop_s}"
+    message = f"{log2.INFO} Exited on {stop_d} at {stop_h}:{stop_m}:{stop_s}.{log2.RESET} "
+    return name, message
 
-"""
-Points of improvement:
-- Export:
-    - Do not export empty history
-- Import:
-    - History imports full AND last two split
-"""
+# Verify folder existence:
+def folder_check(term, root=get_path()):
+    path = f"{root}/{term}"
+    if not isdir(path):
+        print(log2.Exporting.Export_FolderMissing(term))
+        makedirs(path)
+    return path
+
+# History file creation:
+def history_dump(content, path):
+    with open(path, 'x') as file:
+        dump(content, file, ensure_ascii=False, indent=2)
+    print(log2.Exporting.Export_Success(path))
+
+# History export logic:
+def history_export(history):
+    filename, message = quit_handling()
+    if history:
+        path = folder_check(term="history")
+        location1, location2 = f"{path}/hist-{filename}.json", f"{path}/hist-{filename}_02.json"
+        for location in {location1, location2}:
+            try:
+                history_dump(history, f"{path}/hist-2026-08-29-001820.json")
+                return exit(message)
+            except FileExistsError as err:
+                print(log2.Exporting.Export_Error(err.filename))
+            except Exception as err:
+                print(log2.Exporting.Export_Error(location))
+                print("An exception occured:", err)
+            print(log2.Exporting.Export_Alternative)
+        print(log2.Exporting.Export_Failure)
+    else:
+        print(log2.Exporting.Export_Empty)
+    exit(message)
