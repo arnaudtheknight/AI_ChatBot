2a694076484f358eb4d96bc302a2c8b4f98483e5
arnaudtheknight <arnaudtheknight@gmail.com>
2026-08-29 22:04:59 +0200

    minor tweaks and QOL improvements

--- modules/model_list.py
+++ modules/model_list.py
@@ -1,16 +1,19 @@
+# Imports:
 from ollama import list as ollama_list
 
-def model_lister():
-    models_list, models_list_edited= [], []
+# Model lister:
+def list_models():
+    list_official, list_edited= [], []
     response = ollama_list()
     for item in response.models:
-        name = str(item.model)
-        if '/' not in name:
-            models_list += [name]
+        name = str(item.model) # call str() to use split() later
+        if "hf.co/" not in name:
+            list_official += [name] # ModelName:version
             name_split = name.split(':', 1)[0]
-            models_list_edited += [name_split.lower()]
-    return sorted(models_list), sorted(models_list_edited)
+            list_edited += [name_split.lower()] # modelname
+    return sorted(list_official), sorted(list_edited)
 
 if __name__ == "__main__":
-    models, models_edited = model_lister()
-    print(sorted(models))
+    official, edited = list_models()
+    print(official)
+    print(edited)
