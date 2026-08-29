af01b81e578d731566ec0f4ff6902398776dc3d7
arnaudtheknight <arnaudtheknight@gmail.com>
2026-08-29 22:39:30 +0200

    fine tuning

--- modules/config.py
+++ modules/config.py
@@ -1,36 +1,34 @@
-# Absolute imports, which work for chat programs
-from modules.classes import log, log2
-from modules.model_list import model_lister
+# Imports:
+## Self-written:
+from modules.classes import log2
+from modules.model_list import list_models
 
+# Yes/No input logic:
 def yesno(string, default_yes=True):
-    answer = input(string).lower()
+    choice = log2.Config.Default_Yes if default_yes else log2.Config.Default_No
     while True:
-        if answer in {'yes', 'y'}:
+        answer = input(f"{string} {choice}").lower()
+        if answer in {"yes", 'y'}:
             return True
-        elif answer in {'no', 'n'}:
+        elif answer in {"no", 'n'}:
             return False
         elif answer == '':
             return True if default_yes else False
-        answer = input(log2.Config_Prompt_Fail)
 
 # Pre-chat parameter settings:
-def configuration(valid, valid_edited):
-    print(log2.Config_Model_Show)
-    print(f"--> {valid}")
-    name = str(input(log2.Config_Model_Prompt))
-    while name.lower() not in (valid_edited or valid):
-        name = str(input(log2.Config_Prompt_Fail))
-    stream = yesno(log2.Config_Mode_Stream, default_yes=False)
-    think = yesno(log2.Config_Mode_Think, default_yes=True)
+def model_config(official, edited):
+    print(log2.Config.Model_List)
+    for model in official: print(f"--> {model}")
+    name = input(log2.Config.Model_Prompt)
+    while (name not in official) and (name.lower() not in edited):
+        name = input(log2.Config.Model_Fail)
+    stream = yesno(log2.Config.Mode_Stream, default_yes=False)
+    think = yesno(log2.Config.Mode_Think, default_yes=True)
     return name, think, stream
 
-# Name/Main doesn't work, because the absolute imports above raise errors for this file
-"""
 if __name__ == '__main__':
-    models, models_edited = model_list()
-    name, think, stream = config(models, models_edited)
+    models, models_edited = list_models()
+    name, think, stream = model_config(models, models_edited)
     print(f"Name: {name}")
     print(f"Think: {think}")
     print(f"Stream: {stream}")
-"""
-
