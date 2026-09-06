627b7364e6a2e192484646b79741c4c7d1670e18
arnaudtheknight <arnaudtheknight@gmail.com>
2026-09-07 00:26:10 +0200

    minor changes

--- modules/config.py
+++ modules/config.py
@@ -4,8 +4,8 @@ from modules.classes import log2
 from modules.model_list import list_models
 
 # Yes/No input logic:
-def yesno(string, default_yes=True):
-    choice = log2.Config.Default_Yes if default_yes else log2.Config.Default_No
+def yesno(string, default=True):
+    choice = log2.Config.Default_Yes if default else log2.Config.Default_No
     while True:
         answer = input(f"{string} {choice}").lower()
         if answer in {"yes", 'y'}:
@@ -13,7 +13,7 @@ def yesno(string, default_yes=True):
         elif answer in {"no", 'n'}:
             return False
         elif answer == '':
-            return True if default_yes else False
+            return True if default else False
 
 # Pre-chat parameter settings:
 def model_config(official, edited):
@@ -22,10 +22,11 @@ def model_config(official, edited):
     name = input(log2.Config.Model_Prompt)
     while (name not in official) and (name.lower() not in edited):
         name = input(log2.Config.Model_Fail)
-    stream = yesno(log2.Config.Mode_Stream, default_yes=False)
-    think = yesno(log2.Config.Mode_Think, default_yes=True)
+    stream = yesno(log2.Config.Mode_Stream, default=False)
+    think = yesno(log2.Config.Mode_Think, default=True)
     return name, think, stream
 
+# Note: this file will not run as is: module import will break
 if __name__ == '__main__':
     models, models_edited = list_models()
     name, think, stream = model_config(models, models_edited)
